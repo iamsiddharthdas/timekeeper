d46b43e3c877264f1a6109fcf385b064404709b2
pacchuu2418 <90840553+pacchuu2418@users.noreply.github.com>
2022-01-20 11:12:42 +0530

    Update Form2.cs

--- WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/Form2.cs
@@ -128,57 +128,56 @@ namespace WindowsFormsApp1
 
         private void btnSubmit_Click(object sender, EventArgs e)
         {
-            //string strTimeSheet = "";
+            string strTimeSheet = "";
 
 
-            //if (bNewRecord == true)
-            //{
-            //    strTimeSheet = String.Concat("INSERT INTO `timekeeper`.`timesheets`(StartDate, TimeSheetCode, Week1Monday, Week1Tuesday, ",
-            //                "Week1Wednesday, Week1Thursday, Week1Friday, Week1Saturday, Week1Sunday, Notes) ",
-            //                "VALUES('", dtpStartDate.Value.ToString("MM/dd/yyyy"), "', '", strTimeSheetCode, "','", txtWeek1Monday.Text, "', '",
-            //                txtWeek1Tuesday.Text, "', '", txtWeek1Wednesday.Text, "', '", txtWeek1Thursday.Text, "', '",
-            //                txtWeek1Friday.Text, "', '", txtWeek1Saturday.Text, "', '",
-            //                txtWeek1Sunday.Text, "', '", txtWeek2Monday.Text, "', '", txtWeek2Tuesday.Text, "', '",
-            //                txtWeek2Wednesday.Text, "', '", txtWeek2Thursday.Text, "', '", txtWeek2Friday.Text, "', '",
-            //                txtWeek2Saturday.Text, "', '", txtWeek2Sunday.Text, "', '", txtNotes.Text, "');");
-            //}
-
-            //if (bNewRecord == false)
-            //{
-            //    strTimeSheet = String.Concat("UPDATE timesheets SET Week1Monday = '", txtWeek1Monday.Text,
-            //                "', Week1Tuesday = '", txtWeek1Tuesday.Text, "', Week1Wednesday = '", txtWeek1Wednesday.Text,
-            //                "', Week1Thursday = '", txtWeek1Thursday.Text, "', Week1Friday = '", txtWeek1Friday.Text,
-            //                "', Week1Saturday = '", txtWeek1Saturday.Text, "', Week1Sunday = '", txtWeek1Sunday.Text,
-            //                "', Week2Monday = '", txtWeek2Monday.Text, "', Week2Tuesday = '", txtWeek2Tuesday.Text,
-            //                "', Week2Wednesday = '", txtWeek2Wednesday.Text, "', Week2Thursday = '", txtWeek2Thursday.Text,
-            //                "', Week2Friday = '", txtWeek2Friday.Text, "', Week2Saturday = '", txtWeek2Saturday.Text,
-            //                "', Week2Sunday = '", txtWeek2Sunday.Text, "', Notes = '", txtNotes.Text,
-            //                "' WHERE TimeSheetCode = '", strTimeSheetCode, "';");
-            //}
-
-            //if (this.ValidTimeSheet == true)
-            //{
-            //    SqlConnection conTimeSheet =
-            //                new SqlConnection("Server = localhost; Database = timekeeper; Uid = root; Pwd =admin;" +
-            //                "Integrated Security=true");
-            //    SqlCommand cmdTimeSheet = new SqlCommand(strTimeSheet, conTimeSheet);
-
-            //    conTimeSheet.Open();
-            //    cmdTimeSheet.ExecuteNonQuery();
-            //    conTimeSheet.Close();
-
-            //    MessageBox.Show("Your time sheet has been submitted");
-            //}
-            //else
-            //{
-            //    MessageBox.Show("The time sheet is not valid\n" +
-            //                     "either you didn't enter a valid employee number, " +
-            //             "or you didn't a valid start date\n" +
-            //             "The time sheet will not be saved");
-            //}
-
-            MessageBox.Show("Successfully updated the timesheet");
+        if (bNewRecord == true)
+        {
+            strTimeSheet = String.Concat("INSERT INTO `timekeeper`.`timesheets`(StartDate, TimeSheetCode, Week1Monday, Week1Tuesday, ",
+                        "Week1Wednesday, Week1Thursday, Week1Friday, Week1Saturday, Week1Sunday, Notes) ",
+                        "VALUES('", dtpStartDate.Value.ToString("MM/dd/yyyy"), "', '", strTimeSheetCode, "','", txtWeek1Monday.Text, "', '",
+                        txtWeek1Tuesday.Text, "', '", txtWeek1Wednesday.Text, "', '", txtWeek1Thursday.Text, "', '",
+                        txtWeek1Friday.Text, "', '", txtWeek1Saturday.Text, "', '",
+                        txtWeek1Sunday.Text, "', '", txtWeek2Monday.Text, "', '", txtWeek2Tuesday.Text, "', '",
+                        txtWeek2Wednesday.Text, "', '", txtWeek2Thursday.Text, "', '", txtWeek2Friday.Text, "', '",
+                        txtWeek2Saturday.Text, "', '", txtWeek2Sunday.Text, "', '", txtNotes.Text, "');");
         }
+
+        if (bNewRecord == false)
+        {
+            strTimeSheet = String.Concat("UPDATE timesheets SET Week1Monday = '", txtWeek1Monday.Text,
+                        "', Week1Tuesday = '", txtWeek1Tuesday.Text, "', Week1Wednesday = '", txtWeek1Wednesday.Text,
+                        "', Week1Thursday = '", txtWeek1Thursday.Text, "', Week1Friday = '", txtWeek1Friday.Text,
+                        "', Week1Saturday = '", txtWeek1Saturday.Text, "', Week1Sunday = '", txtWeek1Sunday.Text,
+                        "', Week2Monday = '", txtWeek2Monday.Text, "', Week2Tuesday = '", txtWeek2Tuesday.Text,
+                        "', Week2Wednesday = '", txtWeek2Wednesday.Text, "', Week2Thursday = '", txtWeek2Thursday.Text,
+                        "', Week2Friday = '", txtWeek2Friday.Text, "', Week2Saturday = '", txtWeek2Saturday.Text,
+                        "', Week2Sunday = '", txtWeek2Sunday.Text, "', Notes = '", txtNotes.Text,
+                        "' WHERE TimeSheetCode = '", strTimeSheetCode, "';");
+        }
+
+        if (this.ValidTimeSheet == true)
+        {
+            SqlConnection conTimeSheet =
+                        new SqlConnection("Server = localhost; Database = timekeeper; Uid = root; Pwd =admin;" +
+                        "Integrated Security=true");
+            SqlCommand cmdTimeSheet = new SqlCommand(strTimeSheet, conTimeSheet);
+
+            conTimeSheet.Open();
+            cmdTimeSheet.ExecuteNonQuery();
+            conTimeSheet.Close();
+
+            MessageBox.Show("Your time sheet has been submitted");
+        }
+        else
+        {
+            MessageBox.Show("The time sheet is not valid\n" +
+                             "either you didn't enter a valid employee number, " +
+                     "or you didn't a valid start date\n" +
+                     "The time sheet will not be saved");
+        }
+
+           
 
         private void btnClose_Click(object sender, EventArgs e)
         {
