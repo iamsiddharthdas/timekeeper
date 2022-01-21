5c15e4e744789066b7520a71437a5af1f354fc34
Siddharth Das <siddharthdas2203@gmail.com>
2022-01-21 12:37:59 +0530

    added current weekview

--- TimeSheet1/TimeSheet1/Form1.Designer.cs
+++ TimeSheet1/TimeSheet1/Form1.Designer.cs
@@ -29,15 +29,12 @@ namespace TimeSheet1
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.addbutton = new System.Windows.Forms.Button();
-            this.savebutton = new System.Windows.Forms.Button();
-            this.closebutton = new System.Windows.Forms.Button();
             this.pid = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.day1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -47,6 +44,19 @@ namespace TimeSheet1
             this.day5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.day6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.day7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addbutton = new System.Windows.Forms.Button();
+            this.savebutton = new System.Windows.Forms.Button();
+            this.closebutton = new System.Windows.Forms.Button();
+            this.l1 = new System.Windows.Forms.Label();
+            this.l2 = new System.Windows.Forms.Label();
+            this.l3 = new System.Windows.Forms.Label();
+            this.l4 = new System.Windows.Forms.Label();
+            this.l5 = new System.Windows.Forms.Label();
+            this.l6 = new System.Windows.Forms.Label();
+            this.l7 = new System.Windows.Forms.Label();
+            this.empname = new System.Windows.Forms.Label();
+            this.startdate = new System.Windows.Forms.Label();
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -54,7 +64,7 @@ namespace TimeSheet1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(683, 140);
+            this.label1.Location = new System.Drawing.Point(647, 39);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(433, 57);
@@ -65,14 +75,14 @@ namespace TimeSheet1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.pid,
@@ -84,33 +94,96 @@ namespace TimeSheet1
             this.day5,
             this.day6,
             this.day7});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle6;
             this.dataGridView1.Location = new System.Drawing.Point(196, 292);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
             this.dataGridView1.Name = "dataGridView1";
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle7;
             this.dataGridView1.RowHeadersWidth = 62;
-            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle8;
             this.dataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.dataGridView1.RowTemplate.Height = 33;
             this.dataGridView1.Size = new System.Drawing.Size(1416, 312);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // pid
+            // 
+            this.pid.HeaderText = "Project ID";
+            this.pid.MinimumWidth = 8;
+            this.pid.Name = "pid";
+            this.pid.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Task";
+            this.Column2.MinimumWidth = 8;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 150;
+            // 
+            // day1
+            // 
+            this.day1.HeaderText = "Day 1";
+            this.day1.MinimumWidth = 8;
+            this.day1.Name = "day1";
+            this.day1.Width = 150;
+            // 
+            // day2
+            // 
+            this.day2.HeaderText = "Day 2";
+            this.day2.MinimumWidth = 8;
+            this.day2.Name = "day2";
+            this.day2.Width = 150;
+            // 
+            // day3
+            // 
+            this.day3.HeaderText = "Day 3";
+            this.day3.MinimumWidth = 8;
+            this.day3.Name = "day3";
+            this.day3.Width = 150;
+            // 
+            // day4
+            // 
+            this.day4.HeaderText = "Day 4";
+            this.day4.MinimumWidth = 8;
+            this.day4.Name = "day4";
+            this.day4.Width = 150;
+            // 
+            // day5
+            // 
+            this.day5.HeaderText = "Day 5";
+            this.day5.MinimumWidth = 8;
+            this.day5.Name = "day5";
+            this.day5.Width = 150;
+            // 
+            // day6
+            // 
+            this.day6.HeaderText = "Day 6";
+            this.day6.MinimumWidth = 8;
+            this.day6.Name = "day6";
+            this.day6.Width = 150;
+            // 
+            // day7
+            // 
+            this.day7.HeaderText = "Day 7";
+            this.day7.MinimumWidth = 8;
+            this.day7.Name = "day7";
+            this.day7.Width = 150;
             // 
             // addbutton
             // 
@@ -123,7 +196,6 @@ namespace TimeSheet1
             this.addbutton.Text = "Add";
             this.addbutton.UseVisualStyleBackColor = true;
             this.addbutton.Click += new System.EventHandler(this.button1_Click);
-            this.addbutton.Enabled = true;
             // 
             // savebutton
             // 
@@ -149,74 +221,111 @@ namespace TimeSheet1
             this.closebutton.UseVisualStyleBackColor = true;
             this.closebutton.Click += new System.EventHandler(this.button4_Click);
             // 
-            // pid
+            // l1
             // 
-            this.pid.HeaderText = "Project ID";
-            this.pid.MinimumWidth = 8;
-            this.pid.Name = "pid";
-            this.pid.Width = 150;
+            this.l1.AutoSize = true;
+            this.l1.Location = new System.Drawing.Point(596, 258);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(12, 20);
+            this.l1.TabIndex = 6;
+            this.l1.Text = ".";
             // 
-            // Column2
+            // l2
             // 
-            this.Column2.HeaderText = "Task";
-            this.Column2.MinimumWidth = 8;
-            this.Column2.Name = "Column2";
-            this.Column2.Width = 150;
+            this.l2.AutoSize = true;
+            this.l2.Location = new System.Drawing.Point(744, 258);
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(12, 20);
+            this.l2.TabIndex = 7;
+            this.l2.Text = ".";
             // 
-            // day1
+            // l3
             // 
-            this.day1.HeaderText = "Monday";
-            this.day1.MinimumWidth = 8;
-            this.day1.Name = "day1";
-            this.day1.Width = 150;
+            this.l3.AutoSize = true;
+            this.l3.Location = new System.Drawing.Point(893, 258);
+            this.l3.Name = "l3";
+            this.l3.Size = new System.Drawing.Size(12, 20);
+            this.l3.TabIndex = 8;
+            this.l3.Text = ".";
             // 
-            // day2
+            // l4
             // 
-            this.day2.HeaderText = "Tuesday";
-            this.day2.MinimumWidth = 8;
-            this.day2.Name = "day2";
-            this.day2.Width = 150;
+            this.l4.AutoSize = true;
+            this.l4.Location = new System.Drawing.Point(1029, 258);
+            this.l4.Name = "l4";
+            this.l4.Size = new System.Drawing.Size(12, 20);
+            this.l4.TabIndex = 9;
+            this.l4.Text = ".";
             // 
-            // day3
+            // l5
             // 
-            this.day3.HeaderText = "Wednesday";
-            this.day3.MinimumWidth = 8;
-            this.day3.Name = "day3";
-            this.day3.Width = 150;
+            this.l5.AutoSize = true;
+            this.l5.Location = new System.Drawing.Point(1182, 258);
+            this.l5.Name = "l5";
+            this.l5.Size = new System.Drawing.Size(12, 20);
+            this.l5.TabIndex = 10;
+            this.l5.Text = ".";
             // 
-            // day4
+            // l6
             // 
-            this.day4.HeaderText = "Thursday";
-            this.day4.MinimumWidth = 8;
-            this.day4.Name = "day4";
-            this.day4.Width = 150;
+            this.l6.AutoSize = true;
+            this.l6.Location = new System.Drawing.Point(1339, 258);
+            this.l6.Name = "l6";
+            this.l6.Size = new System.Drawing.Size(12, 20);
+            this.l6.TabIndex = 11;
+            this.l6.Text = ".";
             // 
-            // day5
+            // l7
             // 
-            this.day5.HeaderText = "Friday";
-            this.day5.MinimumWidth = 8;
-            this.day5.Name = "day5";
-            this.day5.Width = 150;
+            this.l7.AutoSize = true;
+            this.l7.Location = new System.Drawing.Point(1481, 258);
+            this.l7.Name = "l7";
+            this.l7.Size = new System.Drawing.Size(12, 20);
+            this.l7.TabIndex = 12;
+            this.l7.Text = ".";
             // 
-            // day6
+            // empname
             // 
-            this.day6.HeaderText = "Saturday";
-            this.day6.MinimumWidth = 8;
-            this.day6.Name = "day6";
-            this.day6.Width = 150;
+            this.empname.AutoSize = true;
+            this.empname.Location = new System.Drawing.Point(675, 144);
+            this.empname.Name = "empname";
+            this.empname.Size = new System.Drawing.Size(130, 20);
+            this.empname.TabIndex = 13;
+            this.empname.Text = "Employee Name : ";
+            this.empname.Click += new System.EventHandler(this.label2_Click);
             // 
-            // day7
+            // startdate
             // 
-            this.day7.HeaderText = "Sunday";
-            this.day7.MinimumWidth = 8;
-            this.day7.Name = "day7";
-            this.day7.Width = 150;
+            this.startdate.AutoSize = true;
+            this.startdate.Location = new System.Drawing.Point(722, 191);
+            this.startdate.Name = "startdate";
+            this.startdate.Size = new System.Drawing.Size(83, 20);
+            this.startdate.TabIndex = 14;
+            this.startdate.Text = "Start Date :";
+            // 
+            // dtpStartDate
+            // 
+            this.dtpStartDate.Location = new System.Drawing.Point(811, 186);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(269, 27);
+            this.dtpStartDate.TabIndex = 15;
+            this.dtpStartDate.ValueChanged += new System.EventHandler(this.dtpStartDate_ValueChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1839, 896);
+            this.Controls.Add(this.dtpStartDate);
+            this.Controls.Add(this.startdate);
+            this.Controls.Add(this.empname);
+            this.Controls.Add(this.l7);
+            this.Controls.Add(this.l6);
+            this.Controls.Add(this.l5);
+            this.Controls.Add(this.l4);
+            this.Controls.Add(this.l3);
+            this.Controls.Add(this.l2);
+            this.Controls.Add(this.l1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.closebutton);
             this.Controls.Add(this.savebutton);
@@ -240,6 +349,16 @@ namespace TimeSheet1
         private System.Windows.Forms.Button addbutton;
         private System.Windows.Forms.Button savebutton;
         private System.Windows.Forms.Button closebutton;
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.Label l2;
+        private System.Windows.Forms.Label l3;
+        private System.Windows.Forms.Label l4;
+        private System.Windows.Forms.Label l5;
+        private System.Windows.Forms.Label l6;
+        private System.Windows.Forms.Label l7;
+        private System.Windows.Forms.Label empname;
+        private System.Windows.Forms.Label startdate;
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
         private System.Windows.Forms.DataGridViewTextBoxColumn pid;
         private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.DataGridViewTextBoxColumn day1;
--- TimeSheet1/TimeSheet1/Form1.cs
+++ TimeSheet1/TimeSheet1/Form1.cs
@@ -18,45 +18,79 @@ namespace TimeSheet1
 
         
         public Form1()
-            {
+        {
                 InitializeComponent();
-            }
+        }
 
-            private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-            {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            day1.HeaderText = dtpStartDate.Value.AddDays(0).DayOfWeek.ToString();
+            day2.HeaderText = dtpStartDate.Value.AddDays(1).DayOfWeek.ToString();
+            day3.HeaderText = dtpStartDate.Value.AddDays(2).DayOfWeek.ToString();
+            day4.HeaderText = dtpStartDate.Value.AddDays(3).DayOfWeek.ToString();
+            day5.HeaderText = dtpStartDate.Value.AddDays(4).DayOfWeek.ToString();
+            day6.HeaderText = dtpStartDate.Value.AddDays(5).DayOfWeek.ToString();
+            day7.HeaderText = dtpStartDate.Value.AddDays(6).DayOfWeek.ToString();
+        }
 
-            }
+        private void dtpStartDate_ValueChanged(object sender, EventArgs e)
+        {
 
 
-            private void button1_Click(object sender, EventArgs e)
-            {
+                l1.Text = dtpStartDate.Value.AddDays(0).ToString("M");
+                l2.Text = dtpStartDate.Value.AddDays(1).ToString("M");
+                l3.Text = dtpStartDate.Value.AddDays(2).ToString("M");
+                l4.Text = dtpStartDate.Value.AddDays(3).ToString("M");
+                l5.Text = dtpStartDate.Value.AddDays(4).ToString("M");
+                l6.Text = dtpStartDate.Value.AddDays(5).ToString("M");
+                l7.Text = dtpStartDate.Value.AddDays(6).ToString("M");
 
-                MySqlConnection con = new MySqlConnection("Server = localhost; Database = timekeeper; Uid = root; Pwd =admin;");
-                con.Open();
+                day1.HeaderText = dtpStartDate.Value.AddDays(0).DayOfWeek.ToString();
+                day2.HeaderText = dtpStartDate.Value.AddDays(1).DayOfWeek.ToString();
+                day3.HeaderText = dtpStartDate.Value.AddDays(2).DayOfWeek.ToString();
+                day4.HeaderText = dtpStartDate.Value.AddDays(3).DayOfWeek.ToString();
+                day5.HeaderText = dtpStartDate.Value.AddDays(4).DayOfWeek.ToString();
+                day6.HeaderText = dtpStartDate.Value.AddDays(5).DayOfWeek.ToString();
+                day7.HeaderText = dtpStartDate.Value.AddDays(6).DayOfWeek.ToString();
+        }
 
-                Form2 f2 = new Form2();
-                f2.ShowDialog();
-                con.Close();
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
 
-            }
 
-            private void button2_Click(object sender, EventArgs e)
-            {
+        private void button1_Click(object sender, EventArgs e)
+        {
 
             MySqlConnection con = new MySqlConnection("Server = localhost; Database = timekeeper; Uid = root; Pwd =admin;");
             con.Open();
-            MySqlCommand cmd = new MySqlCommand("UPDATE project` SET `day1` = '8', `day2` = '9', `day3` = '9' WHERE(`project_id` = pid);");
+
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+            con.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        MySqlConnection con = new MySqlConnection("Server = localhost; Database = timekeeper; Uid = root; Pwd =admin;");
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand("UPDATE project` SET `day1` = '8', `day2` = '9', `day3` = '9' WHERE(`project_id` = pid);");
 
 
             
         }
 
-            private void button4_Click(object sender, EventArgs e)
-            {
-                Close();
-            }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
 
-        private void Form1_Load(object sender, EventArgs e)
+        
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
